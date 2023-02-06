6160a0fdc07dc08083b38cdd44a1d82aa1334914
Kody Neldner <kodyneldner@Kodys-Air.crescentschool.local>
2023-02-06 16:42:00 -0500

    Added auto path chooser

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,19 +6,23 @@ package frc.robot;
 
 import static frc.robot.Constants.*;
 
+import frc.robot.commands.A_Test_Path;
 import frc.robot.commands.T_Drivetrain_ArcadeDrive;
 import frc.robot.subsystems.Drivetrain;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 public class RobotContainer {
+  SendableChooser<Command> autoChooser_m = new SendableChooser<>();
   public static CommandXboxController driver_s;
   public static CommandXboxController operator_s;
 
   public static Drivetrain drivetrainInst_s;
 
   public RobotContainer() {
+    autoChooser_m.setDefaultOption("Test Path", new A_Test_Path());
+
     driver_s = new CommandXboxController(PORT_DRIVER);
     operator_s = new CommandXboxController(PORT_OPERATOR);
 
@@ -33,6 +37,6 @@ public class RobotContainer {
   }
 
   public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
+    return autoChooser_m.getSelected();
   }
 }
