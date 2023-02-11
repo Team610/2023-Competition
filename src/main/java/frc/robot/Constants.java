3ef87960363aa392b5202ad0c9f03a1e48dc7ad3
David Shan <davidshan20070302@gmail.com>
2023-02-10 19:28:08 -0500

    added PID thing

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+
 public final class Constants {
     public static final int PORT_DRIVER = 0;
     public static final int PORT_OPERATOR = 1;
@@ -12,9 +14,34 @@ public final class Constants {
         public static final int CAN_RIGHT_ROBIN = 2;
         public static final int CAN_PIDGEY = 1;
 
-        public static final double UNIT_TICKS_PER_REV = 21580;
-        public static final double UNIT_DIST_PER_REV = 0.4930;
+        // units are in meters for Ramsete
 
+        public static final double VAL_KS = 0.063267;
+        public static final double VAL_KV = 3.4115;
+        public static final double VAL_KA = 0.20518;
+        public static final double VAL_KP = 1.4449;
+        public static final double VAL_KD = 0.0;
+        public static final double VAL_KI = 0.0;
+        
+
+        public static final double VAL_TRACK_WIDTH = 0.59492;
+        public static final DifferentialDriveKinematics DRIVE_KINEMATICS = new DifferentialDriveKinematics(VAL_TRACK_WIDTH);
+
+        public static final double VAL_WHEEL_DIA = 0.1524;
+        public static final double VAL_MAX_VELO = 1.93;
+        public static final double VAL_MAX_ACCEL = VAL_MAX_VELO*0.5;
+        public static final double VAL_MAX_VOLTS = 10; // TODO: change to 12 for later
+
+        public static final double VAL_RAMSETE_B = 1;
+        public static final double VAL_RAMSETE_ZETA = 0.7;
+        
+        public static final double UNIT_FEET_IN_METERS = 3.280;
+        public static final double UNIT_METERS_IN_FEET = 0.305;
+        public static final double UNIT_TICKS_PER_REV = 29500;
+        public static final double UNIT_DIST_PER_REV = 0.47878;
+        
         public static final double VAL_DEADBAND = 0.03;
+
+        public static final double VAL_GRAVVECTOR[] = {0,0,1};
     }
 }
--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import static frc.robot.Constants.*;
 import static frc.robot.Constants.Drivetrain.*;
+import edu.wpi.first.math.controller.PIDController;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -22,6 +23,7 @@ public class Drivetrain extends Subsystem610 {
     private WPI_TalonFX leftBatman_m, leftRobin_m, rightBatman_m, rightRobin_m;
     private DifferentialDriveOdometry odometry_m;
     private WPI_Pigeon2 pidgey_m;
+    private PIDController pid_m;
 
     private Drivetrain() {
         super("Drivetrain");
@@ -31,6 +33,9 @@ public class Drivetrain extends Subsystem610 {
         rightRobin_m = MotorConfig.configDriveFollower(CAN_RIGHT_ROBIN, CAN_RIGHT_BATMAN, true, false);
 
         pidgey_m = new WPI_Pigeon2(CAN_PIDGEY, CAN_BUS_NAME);
+
+        pid_m  = new PIDController(VAL_KP, VAL_KI, VAL_KD);
+
 
         odometry_m = new DifferentialDriveOdometry(Rotation2d.fromDegrees(0), getLeftMeters(), getRightMeters());
     }
@@ -42,9 +47,24 @@ public class Drivetrain extends Subsystem610 {
         return driveInst_s;
     }
 
+     /**
+     * Calibrate Pigeon 2.0 
+     */
+    public void calibratePidgey(){
+        pidgey_m.calibrate();
+    }
+
+    /**
+     * Get gravity vector
+     */
+    public double getRoll(){
+        return pidgey_m.getRoll();
+    }
+
     /**
      * Sets all drivetrain motors to coast mode
      */
+
     public void setCoast() {
         leftBatman_m.setNeutralMode(NeutralMode.Coast);
         leftRobin_m.setNeutralMode(NeutralMode.Coast);
@@ -101,7 +121,6 @@ public class Drivetrain extends Subsystem610 {
     public void setRight(ControlMode mode, double output) {
         rightBatman_m.set(mode, output);
     }
-
     /**
      * Sets the robot to drive based on a voltage number, uses setLeft and setRight methods to do it
      * @param leftVolts the desired voltage for the left motor
@@ -157,6 +176,12 @@ public class Drivetrain extends Subsystem610 {
      */
     public Pose2d getPose() {
         return odometry_m.getPoseMeters();
+    }
+
+    public void adjustPID(){
+        pid_m.setTolerance(1.4, 0);
+        driveInst_s.setLeft(-pid_m.calculate(pidgey_m.getYaw(), 0));
+        driveInst_s.setRight(pid_m.calculate(pidgey_m.getYaw(), 0));
     }
 
     @Override
