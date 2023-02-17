49c583b2959608078b2dd34b1713bf83dd3b3b02
Kody Neldner <kodyneldner@Kodys-Air.crescentschool.local>
2023-02-17 09:40:56 -0500

    redid logic for when operator can move

--- src/main/java/frc/robot/commands/T_Cascade_Move.java
+++ src/main/java/frc/robot/commands/T_Cascade_Move.java
@@ -20,9 +20,14 @@ public class T_Cascade_Move extends CommandBase {
      */
     @Override
     public void execute() {
-        if(!cascadeInst_m.getSafety() && !cascadeInst_m.cascadeTopLimitCheck()){
-            double speed = MathUtil.applyDeadband(RobotContainer.operator_s.getLeftY() * VAL_MAX_SPEED, 0.02);
-            cascadeInst_m.spin(speed);
+        if(!cascadeInst_m.getSafety()){
+            if(cascadeInst_m.cascadeTopLimitCheck()){
+                double speed = MathUtil.applyDeadband(RobotContainer.operator_s.getLeftY() * VAL_MAX_SPEED, 0.02);
+                cascadeInst_m.spin(-Math.abs(speed));
+            } else {
+                double speed = MathUtil.applyDeadband(RobotContainer.operator_s.getLeftY() * VAL_MAX_SPEED, 0.02);
+                cascadeInst_m.spin(speed);
+            }
         }
     }
 
