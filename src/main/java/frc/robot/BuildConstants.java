fed04697fafd9a23d1e539ebf44f9eb2f076a5f1
JacksonDing123 <122178040+JacksonDingJD@users.noreply.github.com>
2023-02-17 18:19:12 -0500

    build constant commit

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2023-Competition";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 49;
-  public static final String GIT_SHA = "6c8ba48f0c20c4ee98ade82cedde83537473c642";
-  public static final String GIT_DATE = "2023-02-17 17:27:54 EST";
+  public static final int GIT_REVISION = 50;
+  public static final String GIT_SHA = "174666e79b9f51b456ece0bbdf7a9d1a78b5b446";
+  public static final String GIT_DATE = "2023-02-17 18:16:15 EST";
   public static final String GIT_BRANCH = "Charge-Station";
-  public static final String BUILD_DATE = "2023-02-17 18:15:18 EST";
-  public static final long BUILD_UNIX_TIME = 1676675718460L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2023-02-17 18:18:37 EST";
+  public static final long BUILD_UNIX_TIME = 1676675917604L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
