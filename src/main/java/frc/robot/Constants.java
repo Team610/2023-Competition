be385a605c2f8d136a59d446fa926cff9e8063a4
Kody Neldner <kodyneldner@Kodys-MacBook-Air.local>
2023-02-28 23:07:38 -0500

    ramp preset update

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -63,7 +63,7 @@ public final class Constants {
         public static final double VAL_MAX_ACCEL_UP = 15000;
         public static final double VAL_CRUISE_VELO_DOWN = 9666;  // Ticks per 100ms
         public static final double VAL_MAX_ACCEL_DOWN = 9666;
-        public static final int VAL_CONCERT_TICKS = 1450; //Tick to percent conversion factor
+        public static final int VAL_CONVERT_TICKS = 1450; //Tick to percent conversion factor
         
         public static final double VAL_KP_DOWN = 0.153;  // Slot 0
         public static final double VAL_KI_DOWN = 0;
@@ -87,7 +87,7 @@ public final class Constants {
         // Preset units are in ticks
         public static final double VAL_MID_PRESET = CALC_TICKS(34);
         public static final double VAL_HIGH_PRESET = CALC_TICKS(46);
-        public static final double VAL_RAMP_PRESET = 14000;
+        public static final double VAL_RAMP_PRESET = 6700;  //14000;
         public static final double VAL_GROUND_PRESET = 1940;
         public static final double VAL_TRANSPORT_PRESET = 14000;
         public static final double VAL_LINEUP_PRESET = 50000;
@@ -119,11 +119,12 @@ public final class Constants {
         public static final double VAL_KD = 0;
         public static final double VAL_KF = 0.4;
 
-        public static final double VAL_ANGLE_RAMP = 16000;  // ticks
+        public static final double VAL_ANGLE_RAMP = 14000;  // ticks
         public static final double VAL_ANGLE_SCORE = 0;
         public static final double VAL_ANGLE_GROUND_INIT = 27500;  // angle to pickup from ground start
         public static final double VAL_ANGLE_GROUND_FINAL = VAL_ANGLE_GROUND_INIT+1800;  // angle to pickup from ground finish
         public static final double VAL_ANGLE_TRANSPORT = 10000;
+        public static final double VAL_ANGLE_HYBRID = 18000;
     }
 
     public static class Intake {
