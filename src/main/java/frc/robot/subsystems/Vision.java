265addef892af7a4cad1de622e07bfe46b8309a9
Adam <adamomarali37@gmail.com>
2023-03-08 19:16:34 -0500

    limelight pid aim

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -4,6 +4,7 @@ import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.cscore.CvSink;
 import edu.wpi.first.cscore.CvSource;
 import edu.wpi.first.cscore.HttpCamera;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.util.sendable.SendableBuilder;
@@ -13,12 +14,18 @@ import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.util.Subsystem610;
 
+import static frc.robot.Constants.Vision.*;
+
+
 public class Vision extends Subsystem610{
     private static Vision visionInst_s;
+    private static Drivetrain drivetrain_m;
 
     private int ledMode_m;
     private int tv_m;
     private NetworkTable networkTable_m;
+    private PIDController pid_m;
+    private boolean isAimed_m;
 
     public static Vision getInstance() {
         if (visionInst_s == null)
@@ -30,6 +37,7 @@ public class Vision extends Subsystem610{
         super("Limelight");
         
         ledMode_m = 0;
+        drivetrain_m = Drivetrain.getInstance();
 
         ShuffleboardTab visionTab = Shuffleboard.getTab("test");
 
@@ -42,6 +50,9 @@ public class Vision extends Subsystem610{
         //make sure the pipeline team number is set to 610
         networkTable_m = NetworkTableInstance.getDefault().getTable("limelight");
         networkTable_m.getEntry("ledMode").setNumber(ledMode_m);
+        pid_m = new PIDController(VAL_KP, VAL_KI, VAL_KD);
+
+
         }
 
     public int getLedMode() {
@@ -70,6 +81,22 @@ public class Vision extends Subsystem610{
      */
     public double calcDistance(){
         return tv_m == 0 ? 0 : 209.0 / Math.tan(Math.toRadians(21 + calcTy()));
+    }
+
+    /**
+     * @return Boolean if the limelight is within the set threshold range
+     */
+    public boolean checkAim(){
+        return isAimed_m = Math.abs(calcTx()) < 2 && tv_m > 0;
+    }
+
+    /**
+     * Use WPILib PID to move the robot to the desired direction
+     */
+    public void aim() {
+        pid_m.setTolerance(1.4, 0);
+        drivetrain_m.setLeft(-pid_m.calculate(calcTx(), 0)*0.3);
+        drivetrain_m.setRight(pid_m.calculate(calcTx(), 0)*0.3);
     }
 
     public void writeDashboard(){
